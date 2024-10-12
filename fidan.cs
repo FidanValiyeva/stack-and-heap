4877f75f98158a39f8bad1075e8169b534e5a004
FidanValiyeva <fidanxv-bp215@code.edu.az>
2024-10-12 09:10:04 +0400

    first commit

---
+++ fidan.cs
@@ -0,0 +1,18 @@
+sing System;
+
+class Program
+{
+    static void Main(string[] args)
+
+{
+    int[] arry = {1,2,3,4,5,6};
+    InsertArray(ref arry,12,23,34);
+}
+
+static void InsertArray (ref int [] arry , params int [] arry2)
+{
+    int []arr = new int [2];
+    Consele.WriteLine(arr[2]);
+    Console.WriteLine();
+    
+}
